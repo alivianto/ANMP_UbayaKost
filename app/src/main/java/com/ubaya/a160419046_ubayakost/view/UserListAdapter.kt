d39ac59d2c79ecf08c553cff8c1d67025501e0fc
Andi L <alivianto99@gmail.com>
2022-06-29 08:17:25 +0700

    Implementasi data binding pada user list

--- app/src/main/java/com/ubaya/a160419046_ubayakost/view/UserListAdapter.kt
+++ app/src/main/java/com/ubaya/a160419046_ubayakost/view/UserListAdapter.kt
@@ -8,27 +8,29 @@ import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.ubaya.a160419046_ubayakost.GlobalData
 import com.ubaya.a160419046_ubayakost.R
+import com.ubaya.a160419046_ubayakost.databinding.CardUserItemBinding
 import com.ubaya.a160419046_ubayakost.model.User
 import com.ubaya.a160419046_ubayakost.util.loadImage
 import kotlinx.android.synthetic.main.card_kost_item.view.*
 import kotlinx.android.synthetic.main.card_user_item.view.*
 
 class UserListAdapter(val userList:ArrayList<User>) : RecyclerView.Adapter<UserListAdapter.UserViewHolder>() {
-    class UserViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+    class UserViewHolder(var view: CardUserItemBinding) : RecyclerView.ViewHolder(view.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.card_user_item,parent,false)
+        val view = CardUserItemBinding.inflate(inflater, parent, false)
 
-        return UserListAdapter.UserViewHolder(view)
+        return UserViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
         val arrayuser = userList[position]
-        with(holder.view){
-            textViewUserName.text = arrayuser.name
-            textViewEmail.text = arrayuser.email
-        }
+        holder.view.user = userList[position]
+//        with(holder.view){
+//            textViewUserName.text = arrayuser.name
+//            textViewEmail.text = arrayuser.email
+//        }
     }
 
     override fun getItemCount() = userList.size
